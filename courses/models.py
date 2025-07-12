fa999783bef9e9003ab6be19bd786a7ba75806eb
h-dixit <129261007+H-Dixit@users.noreply.github.com>
2025-07-12 22:15:55 +0530

    add course app

---
+++ courses/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=500, null=True, blank=True)  # Optional field for short description
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    difficulty_level = models.CharField(max_length=50, choices=[
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+    ])
+    image_url = models.URLField(max_length=200, blank=True, null=True)  # Optional field for image URL
+    video_url = models.URLField(max_length=200, blank=True, null=True)
+    published_date = models.DateField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    duration = models.DurationField()  # Duration in hours, minutes, etc.
+    instructor = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    prerequisites = models.TextField(blank=True, null=True)  # Optional field for prerequisites
+    
+
+    def __str__(self):
+        return self.title
+
